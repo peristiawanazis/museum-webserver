8c480304e91b87621f186bf83113fb7143d8626d
Azis <iazis@int.cggveritas.com>
2016-04-19 01:41:24 +0700

    Mainpage panel side has been added

--- application/views/main.php
+++ application/views/main.php
@@ -4,6 +4,8 @@
 	<title>Main Page</title>
   <!-- Bootstrap -->
     <link href="<?=base_url()?>/css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?=base_url()?>/css/layout_mainpage.css" rel="stylesheet">
+
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?=base_url()?>/js/bootstrap.min.js"></script>
      <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
@@ -24,11 +26,56 @@
 		position: absolute;
 		left: 50%;
 	}
+.container_bg{
+  position: absolute;
+  background-color: red;
+  width: 100%;
+  height: 80%;
+  margin-top: 50Px;
+}
+
 
 </style>
 
 </head>
 <body>
+
+   <div id="wrapper">
+
+        <!-- Sidebar -->
+        <div id="sidebar-wrapper">
+            <ul class="sidebar-nav">
+                <li class="sidebar-brand">
+                   
+                </li>
+                <li>
+                    <a href="#">Dashboard</a>
+                </li>
+                <li>
+                    <a href="#">Shortcuts</a>
+                </li>
+                <li>
+                    <a href="#">Overview</a>
+                </li>
+                <li>
+                    <a href="#">Events</a>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                </li>
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </div>
+        <!-- /#sidebar-wrapper -->
+
+       
+
+ <!-- Header Content -->
 <nav class="navbar navbar-default">
   <div class="container-fluid">
     <div class="navbar-header" >
@@ -37,8 +84,28 @@
       <img src="<?=base_url()?>/img/logo.png" style="height: 45px; width: 45px; float: right;" class="img-circle" id="logo" >
   </div>
   </div>
-  <nav>
-  <!-- end of nav bar-->
+  </nav>
+
+   <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <div class="container-fluid">
+                <div class="row">
+                   <a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+    <!-- /#wrapper -->
+     <!-- Menu Toggle Script -->
+    <script>
+    $("#menu-toggle").click(function(e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("toggled");
+    });
+    </script>
 
 </body>
 </html>
+
